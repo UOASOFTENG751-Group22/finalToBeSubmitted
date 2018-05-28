f021ec9c4e9da530734d2092747c6e4dd0b1a7a0
kangguolong <kangguolong@gmail.com>
2018-05-28 19:21:35 +1200

    Add files via upload

---
+++ plotting.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import os
+from shutil import rmtree
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob
+
+problem_sizes = [20, 30, 40, 50]
+data_source_path = './result/'
+plot_folder = './plot'
+
+frame = DataFrame()
+source_list = []
+for data_source in glob.glob(data_source_path + '*.csv'):
+    data_set = pd.read_csv(data_source)
+    source_list.append(data_set)
+    
+if os.path.exists(plot_folder):
+	rmtree(plot_folder)
+os.mkdir(plot_folder)
+
+frame = pd.concat(source_list)
+
+grouped = frame['Exe_Time'].groupby([frame['Execution_Kind'], frame['Problem_Size']])
+mean_ds = grouped.mean().reset_index()
+exe_time_plot = pd.pivot_table(mean_ds, index='Problem_Size', columns ='Execution_Kind')
+fig = plt.figure()
+fig.set_size_inches(14, 7.5)
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+chart = fig.add_subplot(2, 1, 1)
+chart.set_title('Execution time with increasing problem size')
+chart.set_xlabel('Problem_Size')
+chart.set_ylabel('Execution Time (ms)')
+plt.plot(exe_time_plot, 'o-')
+plt.legend(('Modified_ParaTask','ParaTask','Sequential'), loc='best')
+
+
+filtered_dataset = []
+filtered_frame = DataFrame()
+for problemSize in problem_sizes:
+    seq_time = mean_ds.loc[(mean_ds['Execution_Kind']=='Sequential') & (mean_ds['Problem_Size'] == problemSize),'Exe_Time']
+    parallel_ds = mean_ds.loc[(mean_ds['Execution_Kind']!='Sequential') & (mean_ds['Problem_Size'] == problemSize)]
+    parallel_ds['Speedup'] = seq_time.values[0]/parallel_ds.Exe_Time
+    filtered_dataset.append(parallel_ds)
+
+filtered_frame = pd.concat(filtered_dataset)
+filtered_frame = filtered_frame.drop(['Exe_Time'], axis=1)
+speedup_plot = pd.pivot_table(filtered_frame, index='Problem_Size', columns ='Execution_Kind')
+speedup_chart = fig.add_subplot(2, 1, 2)
+speedup_chart.set_title('speedup with increasing problem size')
+speedup_chart.set_xlabel('Problem_Size')
+speedup_chart.set_ylabel('Speedup')
+plt.plot(speedup_plot, 'o-')
+plt.legend(('Modified_ParaTask','ParaTask'), loc='best')
+plt.savefig(plot_folder + '/analysis.svg', dpi=400, bbox_inches='tight')
+
+    
---
+++ run.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python3
+import os
+from shutil import rmtree
+import subprocess
+
+""" initialize variables """
+problem_sizes = [2000, 4000, 60000, 8000, 10000]
+iterations = 5
+# problem_sizes = [20, 30, 40, 50]
+resultFolder = "result"
+resultFileName = "data_set.csv"
+execution_kinds = ['Sequential', 'ParaTask', 'Modified_ParaTask']
+
+benchmark_java_file = "uoa.se751.group22.benchmark.Benchmark"
+ParaTask = "./ParaTask-Original.jar"
+ParaTask_Modified = "./ParaTask-WorkStealingRuntime-1.0.1.jar"
+
+""" delete exist result folder"""
+if os.path.exists(resultFolder):
+	rmtree(resultFolder)
+
+""" create result folder"""
+os.mkdir(resultFolder)
+
+""" generate data function"""
+def generate_data(file):
+	for problem_size in problem_sizes:
+		write_result(file, problem_size)
+
+""" write result to data_set.csv file"""
+def write_result(file, problem_size):
+	for i in range(iterations + 1):
+		exe_cmd_seq = "java -Xms1024M -Xmx6144M -cp .:%s:ParaTask-BenchmarkRuntime-1.0.0.jar %s SEQUENTIAL %d" %(ParaTask, benchmark_java_file, problem_size)
+		exe_time_seq = subprocess.check_output(exe_cmd_seq, shell=True).decode('utf-8').strip('\r\n')
+		file.write(execution_kinds[0] + "," + str(problem_size) + "," + str(exe_time_seq) + "\n")
+
+		exe_cmd_para = "java -Xms1024M -Xmx6144M -cp .:%s:ParaTask-BenchmarkRuntime-1.0.0.jar %s STEAL %d" %(ParaTask, benchmark_java_file, problem_size)
+		exe_time_para = subprocess.check_output(exe_cmd_para, shell=True).decode('utf-8').strip('\r\n')
+		file.write(execution_kinds[1] + "," + str(problem_size) + "," + str(exe_time_para) + "\n")
+
+		exe_cmd_para_modified = "java -Xms1024M -Xmx6144M -cp .:%s:ParaTask-BenchmarkRuntime-1.0.0.jar %s STEAL %d" %(ParaTask_Modified, benchmark_java_file, problem_size)
+		exe_time_para_modified = subprocess.check_output(exe_cmd_para_modified, shell=True).decode('utf-8').strip('\r\n')
+		file.write(execution_kinds[2] + "," + str(problem_size) + "," + str(exe_time_para_modified) + "\n")
+
+""" open data_set.csv file and write result"""
+print("start running.....")
+with open(resultFolder + "/" + resultFileName, "w") as file:
+	file.write("Execution_Kind,Problem_Size,Exe_Time\n")
+	generate_data(file)
+	
+print("finished generating data finally!")
